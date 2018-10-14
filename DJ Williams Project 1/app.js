bf55d31fec9868237b772b8d85dd8bafd697053d
CB_ATL_110_David_Williams <cb_atl_110_david_williams@MacBook-Air-7.local>
2018-10-14 04:18:07 -0400

    added player image sprite, cant preserve it after button press though.

--- DJ Williams Project 1/app.js
+++ DJ Williams Project 1/app.js
@@ -14,61 +14,63 @@
 // //         }
 // //     }
 
-
-
-// // THE CORRECT CODE SNIPPET! BELOW!
-
-
-
-window.onload = function() {
-    var backgroundContext = document.getElementById("backgroundCanvas").getContext('2d'); // This is code for the background canvas element!
+var backgroundContext = document.getElementById("backgroundCanvas").getContext('2d'); // This is code for the background canvas element!
     
     var gamecanvas = document.querySelector('#gameCanvas'); 
 
     var gamecontext = gamecanvas.getContext('2d');//This is our game canvas space 
     
     var backgroundImg = this.document.getElementById("stars"); //This is javascript grabbing the background image from html and makes it an image element.
-    
-    
-
 
     backgroundContext.drawImage(backgroundImg, 0, 0); //This tells JS to draw the image in the canvas environment.
+
+
+        // Player Parameters!
+
+var Player = {};
+Player_width = 220,
+Player_height = 172,
+Player_img = new Image();
+Player_img.src = 'assets/Spaceship.png';
+
+
+window.onload = function() {
+
 
 var xPos = 0; // postion of box
 var yPos = 0;// postion of box
 
 
 gamecontext.stroke();// draw rectangle on the game canvas space.
+gamecontext.drawImage(Player_img, 0, 0)
 
 
-function move(e) { 
+function move(Player) { 
     // This is for the right directional key
-if(e.keyCode ==39){
+if(Player.keyCode ==39){
     xPos+=70
 }
     // This is for the left directional key 
-if(e.keyCode ==37){
+if(Player.keyCode ==37){
     xPos-=70
 }
    // This is for the up directional key 
 
-if(e.keyCode ==40){
+if(Player.keyCode ==40){
     yPos+=70 
 }
 // This is for the down directional key
 
-if(e.keyCode == 38){
+if(Player.keyCode == 38){
     yPos-=70
 }
 
-// alert(e.keyCode);
-
 gamecanvas.width=gamecanvas.width; //this loops it all 
-gamecontext.fillStyle="white";
 gamecontext.fillRect(xPos,yPos,100,100); // this preserves the squares
 
-
 }
+
+
 
 document.onkeydown = move;
 };
